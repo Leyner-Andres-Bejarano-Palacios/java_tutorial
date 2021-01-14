c8265eac2eb345dbdec7c8abeaee25e42314e1ef
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2021-01-14 01:54:05 -0500

    :bento: :monocle_face: added the exercise 1

---
+++ exercises/1-1_w3resource_basic_1/W3resource_basic_1_num_1.java
@@ -0,0 +1,6 @@
+public class W3resource_basic_1_num_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
