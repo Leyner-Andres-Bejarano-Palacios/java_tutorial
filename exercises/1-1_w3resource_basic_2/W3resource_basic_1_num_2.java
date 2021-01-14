2813b05f047266c73aeb18e7e3310dcae6227c2f
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2021-01-14 02:01:04 -0500

    :bento: :monocle_face: added the exercise 2

---
+++ exercises/1-1_w3resource_basic_2/W3resource_basic_1_num_2.java
@@ -0,0 +1,7 @@
+public class W3resource_basic_1_num_2 {
+    public static void main(String[] args) {
+        System.out.println("74 + 36");
+        System.out.println("Expected Output :");
+        System.out.println(74 + 36);
+    }
+}
