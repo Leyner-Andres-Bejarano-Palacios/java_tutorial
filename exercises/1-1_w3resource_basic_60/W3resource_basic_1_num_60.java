9fcbabc370c8a0e1a3268a4e80b9b2f46445a7f5
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2021-01-16 05:04:42 -0500

    :bento: :monocle_face: added the exercise 60

---
+++ exercises/1-1_w3resource_basic_60/W3resource_basic_1_num_60.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class W3resource_basic_1_num_60 {
+
+
+    public static void main(String[] args) {
+
+      Scanner in = new Scanner(System.in);
+      System.out.print("Input a Sentence: ");
+      String line = in.nextLine();
+      String[] words = line.split("[ ]+");
+      System.out.println("Penultimate word: "+words[words.length - 2]);
+    }
+
+    }
