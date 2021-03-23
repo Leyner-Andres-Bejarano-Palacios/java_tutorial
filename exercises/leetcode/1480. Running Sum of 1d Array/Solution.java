74470c320fbf8e91656774c7f27babb07bb81575
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2021-03-23 08:42:50 -0500

    :building_construction: the 1480 first implementation, missing the documentation where I describe the O notation, and the test

--- exercises/leetcode/1480. Running Sum of 1d Array/Solution.java
+++ exercises/leetcode/1480. Running Sum of 1d Array/Solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] arrayAccumulated = new int[nums.lenght];
+        int[] arrayAccumulated = new int[nums.length];
         int accumt = 0 ;
-        for (int i = 0; i < nums.lenght; i++)
-            accumt = accumt + nums[i]
+        for (int i = 0; i < nums.length; i++){
+            accumt = accumt + nums[i];
             arrayAccumulated[i] = accumt;
-        return arrayAccumulated
+        }
+        return arrayAccumulated;
     } 
 }
